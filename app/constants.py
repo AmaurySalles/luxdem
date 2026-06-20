14abdd42ac743b093082079a8a752a9868a6797d
Amaury Salles <shmiggit@amaurys-mbp.home>
2026-06-20 13:07:15 +0200

    infra cleanup

--- app/constants.py
+++ app/constants.py
@@ -1,9 +1,7 @@
 """
 Global useful constants
 """
-import string
 from pathlib import Path
-from reducto import Reducto
 from ecodev_core import SETTINGS
 
 
@@ -29,54 +27,3 @@ FEEDBACK_URL = 'https://ecosia.com'
 DOCUMENTATION_URL = 'https://ecosia.com'
 
 
-"""
-SECURITY CONSTANTS
-"""
-PWD_CHAR_CHOICES = string.ascii_letters + string.digits + '!?.,;@#~][+=-/*()&^%$'
-PASSWORD_LENGTH = 15
-
-
-"""
-DASH DATA STORES
-"""
-ALERT_STORE = 'alert-store'
-PROJECT_ID_STORE = 'project-id-store'
-USER_DELETION_STORE = 'user-deletion-store'
-
-
-"""
-ACCESS CONSTANTS
-"""
-PROJECT = 'project'
-CREATE_PROJECT = 'create_project'
-
-# Project #
-NAME = 'name'
-DESCRIPTION = 'description'
-PROJECT_ID = 'project_id'
-YEAR = 'year'
-MODIFIED_BY = 'modified_by'
-DEMO = 'demo'
-
-# ProjectAccess #
-USER_ID = 'user_id'
-ROLE = 'role'
-IS_PM = 'is_pm'
-USER = 'user'
-MODULE = 'module'
-
-# ModuleAccess #
-MODULE_ACCESS = 'module_access'
-MODULE_NAME = 'module_name'
-HAS_ACCESS = 'has_access'
-PROJECT_ACCESS_ID = 'project_access_id'
-
-
-"""
-OTHER CALLBACK CONSTANTS
-"""
-# Custom Callback #
-CHECKS = 'checks'
-COMPUTATION = 'computation'
-MONITORING = 'monitoring'
-MONITOR_DETAILS = 'monitor_details'
